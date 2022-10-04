74bc8d45593430018567fd15f598236f54336210
ViniciusCaval123 <vinicius.cavalcanti1719@gmail.com>
2022-10-03 22:09:36 -0300

    commit

--- app.py
+++ app.py
@@ -7,17 +7,18 @@ app = Flask(__name__)
 def index():
     return render_template('forms.html')
 
-@app.route('/api/chato', methods=['POST'])
+@app.route('/api/chato', methods=['POST','GET'])
 def chato():
 
     json = request.get_json()
-    first_name = json['first']
-    last_name = json['last']
-    time_name = json['time']
-    combo = json['combo']
+    first_name = json['first'].upper()
+    last_name = json['last'].upper()
+    time_name = json['time'].upper()
+    combo = json['combo'].upper()
+    print (first_name)
     return jsonify(first=first_name, last=last_name, time=time_name, combo=combo)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5500))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
